3ad6f5f220e8f5bd8ac5efd65b892748d3f172a0
zhanghaijun <zhanghaijun@bjtxra.com>
2019-12-20 10:57:28 +0800

    BlogSetting  @Data

--- BlogService/src/main/java/com/blog/config/BlogSetting.java
+++ BlogService/src/main/java/com/blog/config/BlogSetting.java
@@ -1,15 +1,13 @@
 package com.blog.config;
 
-import lombok.Getter;
-import lombok.Setter;
+import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
 
 @Configuration
 @ConfigurationProperties(prefix="blog")
-@Setter
-@Getter
+@Data
 public class BlogSetting {
 
     private String UIDir;
