da91abd830ce0075a3994998e0fa88ab9eb531d8
zhanghaijun <zhanghaijun@bjtxra.com>
2019-12-30 18:25:51 +0800

    添加 activejdbc

--- BlogService/src/main/java/com/blog/api/UserApi.java
+++ BlogService/src/main/java/com/blog/api/UserApi.java
@@ -1,10 +1,10 @@
 package com.blog.api;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
+import javax.ws.rs.*;
 
 @Path("/demo")
+//@Produces({"application/x-protobuf", "application/json"})
+//@Consumes({"application/x-protobuf", "application/json"})
 public class UserApi {
 
     @GET
