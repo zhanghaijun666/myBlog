584964147a119eb3fd9463ac4b90c63ad1436fa4
zhanghaijun <zhanghaijun@bjtxra.com>
2020-01-09 17:57:42 +0800

    add logback

--- BlogServer/src/main/java/com/blog/config/DataSourceConfig.java
+++ BlogServer/src/main/java/com/blog/config/DataSourceConfig.java
@@ -28,6 +28,9 @@ public class DataSourceConfig {
         dataSource.setUrl(jdbcUrl);
         dataSource.setUsername(jdbcUser);
         dataSource.setPassword(jdbcPassword);
+        dataSource.setMaxActive(200);
+        dataSource.setMaxWait(100);
+        dataSource.setValidationQuery("SELECT 1 FROM dual");
         return dataSource;
     }
 }
