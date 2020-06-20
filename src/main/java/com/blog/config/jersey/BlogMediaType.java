5eceb7aa9b26820f282516e19163556cf82427e3
zhanghaijun <zhanghaijun@bjtxra.com>
2020-06-20 23:32:34 +0800

    HttpMessageConverter

--- src/main/java/com/blog/config/jersey/BlogMediaType.java
+++ src/main/java/com/blog/config/jersey/BlogMediaType.java
@@ -1,8 +1,10 @@
 package com.blog.config.jersey;
 
-
 import javax.ws.rs.core.MediaType;
 
 public class BlogMediaType extends MediaType {
-    public static final String APPLICATION_PROTOBUF = "application/x-protobuf";
+
+    public static final String PROTOBUF_TYPE = "application";
+    public static final String PROTOBUF_SUBTYPE = "x-protobuf";
+    public static final String APPLICATION_PROTOBUF = PROTOBUF_TYPE + "/" + PROTOBUF_SUBTYPE;
 }
--- src/main/java/com/blog/config/mvc/MyMessageConverter.java
+++ src/main/java/com/blog/config/mvc/MyMessageConverter.java
@@ -1,39 +1,47 @@
 package com.blog.config.mvc;
 
+import com.blog.config.jersey.BlogMediaType;
 import com.google.protobuf.Message;
 import lombok.SneakyThrows;
 import org.springframework.http.HttpInputMessage;
 import org.springframework.http.HttpOutputMessage;
 import org.springframework.http.MediaType;
-import org.springframework.http.converter.AbstractHttpMessageConverter;
+import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.http.converter.HttpMessageNotWritableException;
 
 import java.io.IOException;
 import java.lang.reflect.Method;
+import java.util.Collections;
+import java.util.List;
 
-public class MyMessageConverter extends AbstractHttpMessageConverter<Message> {
-
-    public MyMessageConverter() {
-        super(new MediaType("application", "x-protobuf"));
+public class MyMessageConverter implements HttpMessageConverter<Message> {
+    @Override
+    public boolean canRead(Class<?> type, MediaType mediaType) {
+        return Message.class.isAssignableFrom(type);
     }
 
     @Override
-    protected boolean supports(Class<?> type) {
+    public boolean canWrite(Class<?> type, MediaType mediaType) {
         return Message.class.isAssignableFrom(type);
+    }
+
+    @Override
+    public List<MediaType> getSupportedMediaTypes() {
+        return Collections.singletonList(new MediaType(BlogMediaType.PROTOBUF_TYPE, BlogMediaType.PROTOBUF_SUBTYPE));
+
     }
 
     @SneakyThrows
     @Override
-    protected Message readInternal(Class<? extends Message> type, HttpInputMessage httpInputMessage) throws IOException, HttpMessageNotReadableException {
+    public Message read(Class<? extends Message> type, HttpInputMessage httpInputMessage) throws IOException, HttpMessageNotReadableException {
         Method newBuilder = type.getMethod("newBuilder");
         Message.Builder builder = (Message.Builder) newBuilder.invoke(type);
         return builder.mergeFrom(httpInputMessage.getBody()).build();
     }
 
     @Override
-    protected void writeInternal(Message message, HttpOutputMessage httpOutputMessage) throws IOException, HttpMessageNotWritableException {
+    public void write(Message message, MediaType mediaType, HttpOutputMessage httpOutputMessage) throws IOException, HttpMessageNotWritableException {
         message.writeTo(httpOutputMessage.getBody());
     }
-
 }
